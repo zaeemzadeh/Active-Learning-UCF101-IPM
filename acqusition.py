c9f885b65a585b7a31f82a3698830c557b96853d
Alireza <zaeemzadeh@gmail.com>
2018-09-21 13:04:16 -0400

    IPM acq added, now we have e_optimal, a_optimal, d_optimal, inv_cond, and IPM

--- acqusition.py
+++ acqusition.py
@@ -1,6 +1,7 @@
 import torch
 from torch.autograd import Variable
 import numpy as np
+import cupy as cp
 import sklearn.cluster as cl
 
 
@@ -159,4 +160,50 @@ def e_optimal_add_sample(train, pool, score, pooled_idx, alpha, type):
     return sorted_idx[-i]
 
 
+def IPM_add_sample(train, pool, pooled_idx):
+    candidate_samples = range(0, len(pool))         # all samples
+    # candidate_samples = cp.argsort(score)[-100:]     # best samples based on score
+    set_idx = [int(idx) for idx in pooled_idx]
 
+    A_train = [np.ravel(t) for t in train]
+    A_train.extend([np.ravel(pool[i]) for i in set_idx])
+    A_s_mat = cp.array(A_train).transpose()
+    if len(A_s_mat.shape) == 1:
+        A_s_mat = A_s_mat.reshape((-1, 1))
+
+    A_pool = [np.ravel(t) for t in pool]
+    A_mat = cp.array(A_pool).transpose()
+    if len(A_mat.shape) == 1:
+        A_mat = A_mat.reshape((-1, 1))
+
+    #print A_s_mat.shape, A_mat.shape
+    if len(A_s_mat) == 0:
+        A_proj = A_mat
+    else:
+        Proj = cp.matmul(A_s_mat, cp.linalg.pinv(A_s_mat))
+        A_proj = A_mat - cp.matmul(Proj, A_mat)
+
+    u, _, _ = cp.linalg.svd(A_proj, full_matrices=False)
+    first_eig_vec = u[0, :]
+    # calculating MP score
+    #print cp.linalg.norm(Res)
+    correlation = cp.zeros(len(pool))
+    for m in candidate_samples:
+
+        if m in pooled_idx:
+            correlation[m] = 0
+            continue
+
+        correlation[m] = cp.abs(cp.inner(A_mat[:, m], first_eig_vec))
+
+    # finding the best sample
+    objective = correlation
+    sorted_idx = cp.argsort(objective)
+    sorted_idx = cp.flipud(sorted_idx)   # sort in decsending order
+
+    # avoiding duplicates
+    i = 0
+    while sorted_idx[i] in pooled_idx:
+        i += 1
+
+    return sorted_idx[i]
